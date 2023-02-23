9a710ce1f98a2988bf20b7afe89cf9d5bed196e3
Christopher Zorn <czorn@flexport.com>
2023-02-23 08:44:03 -0800

    emacs

---
+++ hello/interface.py
@@ -0,0 +1,4 @@
+# There needs to be a way to bridge or provide a client or agent from
+# consumer and producer. This has to be interchangable.
+# Is an agent interface class the way to go?
+# Twisted defines Interfaces, can this work the same? Java does this too

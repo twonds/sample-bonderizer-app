d272f4b05267994fc3edfbbd7cee5ea6b8ff89c6
Christopher Zorn <czorn@flexport.com>
2023-02-23 08:44:26 -0800

    interface-or-api

---
+++ hello/contract.py
@@ -0,0 +1,6 @@
+# This will be the definition of a contract from consumer to producer
+# It is the shared code to test a contract or ensure consumers and producers
+# adhere their contract and do not break it
+#
+# Test can and should be outside this code but use this code to execute tests
+# 
--- hello/interface.py
+++ hello/interface.py
@@ -2,3 +2,4 @@
 # consumer and producer. This has to be interchangable.
 # Is an agent interface class the way to go?
 # Twisted defines Interfaces, can this work the same? Java does this too
+# https://docs.twisted.org/en/twisted-18.7.0/core/howto/components.html
